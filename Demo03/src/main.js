fa0deea72335978a368a632157b4f27b1be5e6ba
Haowu <2637405542@qq.com>
2021-04-14 19:55:42 +0800

    wh修改框架

--- Demo03/src/main.js
+++ Demo03/src/main.js
@@ -4,12 +4,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
-
+// mint ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+//element ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
 /* eslint-disable no-new */
+Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  el: '#app',
+  render: h => h(App)
 })
--- Demo03/src/router/index.js
+++ Demo03/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import one from '../components/Test1'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/one',
+      name: 'one',
+      component: one
     }
   ]
 })
